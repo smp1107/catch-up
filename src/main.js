f467c4fd41ff094b2867e2203091e032ce6a6b69
alumnos <u202414345@upc.edu.pe>
2026-04-20 11:48:30 -0500

    feat(material-design): integrate PrimeVue components and i18n support in main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './app.vue'
+import i18n from "./i18n.js";
+import PrimeVue from 'primevue/config';
+import Material from '@primeuix/themes/material';
+import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
+import {Avatar, Card, Drawer, Menu, Menubar, SelectButton, Toolbar, Tooltip} from "primevue";
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(i18n)
+    .use(PrimeVue, {ripple:true, theme:{presetP:Material}})
+    .component('pv-button', Button)
+    .component('pv-select-button', SelectButton)
+    .component('pv-avatar', Avatar)
+    .component('pv-drawer', Drawer)
+    .component('pv-card', Card)
+    .component('pv-image', Image)
+    .component('pv-toolbar', Toolbar)
+    .component('pv-menu', Menu)
+    .component('pv-menubar', Menubar)
+    .component('pv-tooltip', Tooltip)
+    .mount('#app')
